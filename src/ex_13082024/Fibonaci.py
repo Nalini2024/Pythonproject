44380cf56d8a33f350ad9ecf3f1843bf120192a8
Nalini2024 <nalini.sadana@gmail.com>
2024-08-22 11:02:46 +0530

    first commit

--- src/ex_13082024/Fibonaci.py
+++ src/ex_13082024/Fibonaci.py
@@ -1,6 +1,17 @@
 # logic
 ''' Task #11 -
 ✅ Fibonaci series 0,0+1, 0+1+1, n = 7 0, 1, 2, 3, 5, 8, 13
-number /3>> "frizz
-   number/5>>> buzz
-    number/3 and 5>>>frizzbuzz'''
+'''
+
+num = int(input("enter a number\n"))
+fib_series = [0, 1]
+
+if num <= 0:
+    print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+    print("The factorial of 0 is 0")
+else:
+    for i in range(2, num):
+        fib_series.append(fib_series[i-1] + fib_series[i-2])
+    print(f"{fib_series}", end="\t")
+
